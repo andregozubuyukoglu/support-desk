95f12ca8542397744cdeeb988f51fa3bc6553e7e
Andre <andregozubuyuk@gmail.com>
2023-05-18 23:50:52 +0300

    Initial commit

--- backend/contollers/userController.js
+++ backend/contollers/userController.js
@@ -1,4 +1,7 @@
 const asyncHandler = require("express-async-handler")
+const bcrypt = require("bcryptjs")
+
+const User = require("../models/userModel")
 
 // @desc Registe a new user
 // @route /api/users
@@ -10,7 +13,35 @@ const registerUser = asyncHandler(async function (req, res) {
     throw new Error("Provide all info")
   }
 
-  res.send("Register Route")
+  //Find the user exists in the database
+  const userExists = await User.findOne({ email })
+
+  if (userExists) {
+    res.status(400)
+    throw new Error("User already exists")
+  }
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  })
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    })
+  } else {
+    res.status(400)
+    throw new error("Invalid user data")
+  }
 })
 
 // @desc Login user
