df4af5dc0eed8891992c4af54363ef8f53aa7478
Andre <andregozubuyuk@gmail.com>
2023-05-18 22:50:51 +0300

    Initial commit

--- backend/contollers/userController.js
+++ backend/contollers/userController.js
@@ -1,8 +1,23 @@
-const registerUser = (req, res) => {
+const asyncHandler = require("express-async-handler")
+
+// @desc Registe a new user
+// @route /api/users
+// @access Public
+const registerUser = asyncHandler(async function (req, res) {
+  const { name, email, password } = req.body
+
+  if (!name || !email || !password) {
+    throw new Error("Provide all info")
+  }
+
   res.send("Register Route")
-}
-const loginUser = (req, res) => {
+})
+
+// @desc Login user
+// @route /api/login
+// @access Public
+const loginUser = asyncHandler(async (req, res) => {
   res.send("Login Route")
-}
+})
 
 module.exports = { registerUser, loginUser }
